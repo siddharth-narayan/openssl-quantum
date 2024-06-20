6fd82de74d6d62d1d6727af971b90b9ec4d114c7
siddharth-narayan <siddharth.060605@gmail.com>
2024-06-20 04:15:54 -0400

    Add static oqsprovider

--- flake.nix
+++ flake.nix
@@ -15,7 +15,9 @@
 	    # Declare some local packages be available via self.packages
       packages.x86_64-linux = {
         default = self.packages.x86_64-linux.openssl-with-oqsprovider;
+        oqsprovider-static = pkgs.callPackage ./oqs-provider.nix { enableStatic = true; };
         oqsprovider = pkgs.callPackage ./oqs-provider.nix {};
+
         openssl-with-oqsprovider = pkgs.callPackage ./openssl-with-providers.nix { providers = [ self.packages.x86_64-linux.oqsprovider ]; };
       };
       devShells.x86_64-linux.default = pkgs.mkShell {
