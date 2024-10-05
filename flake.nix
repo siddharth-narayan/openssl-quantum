83aa0224903bdc2fe53802906f4542b34171030c
siddharth-narayan <siddharth.060605@gmail.com>
2024-10-05 00:21:27 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
+    flake-utils.lib.eachSystem [ system.x86_64-linux system.i686-linux ] (system:
       let pkgs = import nixpkgs { inherit system; };
       in
       {
